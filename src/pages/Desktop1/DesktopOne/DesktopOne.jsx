7c70def37aa5cd58eb5887b1c911c4d2e4d44cb1
Christopher Abraham <christopherabraham8@gmail.com>
2022-12-19 08:10:37 +0100

    finished desktop 1

--- src/pages/Desktop1/DesktopOne/DesktopOne.jsx
+++ src/pages/Desktop1/DesktopOne/DesktopOne.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import Header from '../Header/header'
-import Card from '../../../UI/cardUi/Card'
-import CardHead from '../../../componets/card/Card'
+import React from "react";
+import Header from "../Header/header";
+import Card from "../../../UI/cardUi/Card";
+import CardHead from "../../../componets/card/Card";
+import "./desktopOne.css";
+import RightArrow from "../../../assets/next.png";
 
 const DesktopOne = (props) => {
   return (
-    <div>
+    <div className="main-container">
       <Header />
-
-      <Card>
-      <CardHead/>
-    
-      </Card>
+      <div className="cardsItemes">
+        <Card>
+          <CardHead />
+        </Card>
+        <Card>
+          <CardHead />
+        </Card>
+        <Card>
+          <CardHead />
+        </Card>
+        <Card>
+          <CardHead />
+        </Card>
+      </div>
+      <a href="#">
+        <img src={RightArrow} alt="next" />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default DesktopOne
+export default DesktopOne;
