5d8afd4020d53423cb5627b4e8b44acfba7c401e
Christopher Abraham <christopherabraham8@gmail.com>
2022-12-26 13:34:59 +0100

    finished the input section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import DesktopOne from "./pages/Desktop1/DesktopOne/DesktopOne";
 import DesktopTwo from "./pages/Desktop2/DesktopTwo/DesktopTwo";
-import Cart from "./pages/Cart/Cart"
+import Cart from "./pages/Cart/Cart";
+import Singup from "./pages/Signup/Singup";
 
 function App() {
   return (
     <div>
       {/* <DesktopOne /> */}
-      <DesktopTwo />
+      {/* <DesktopTwo /> */}
       {/* <Cart/> */}
-      
-      
+      <Singup />
     </div>
   );
 }
